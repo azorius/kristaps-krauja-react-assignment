d3a32efbc1f17d48e6c6eb7313e8f7e2935cdf64
Kristaps <azorien@gmail.com>
2022-03-03 15:06:10 +0200

    Add product filtering by category in endpoint request and product details retrieval

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
   state = {
     categories: [],
     products: [],
+    product: {},
   }
   fetchCategories = () => {
     const query = new Query('categories', true).addField('name')
@@ -19,36 +20,64 @@ class App extends React.Component {
     console.log(this.state)
   }
 
-  fetchProducts = () => {
+  fetchProducts = (category) => {
     const query = new Query('category', true)
-      .addArgument('title', 'tech')
+
       .addField('name')
       .addField(new Field('products').addFieldList(['id', 'name', 'category']))
-
+    if (category) {
+      query.addArgument('input', 'CategoryInput', { title: category })
+    }
     client
       .post(query)
       .then((result) => this.setState({ products: result.category.products }))
     console.log(this.state)
   }
 
+  fetchProduct = (id) => {
+    const query = new Query('product', true)
+      .addArgument('id', 'String!', id)
+      .addFieldList(['id', 'name', 'category'])
+
+    client
+      .post(query)
+      .then((result) => this.setState({ product: result.product }))
+    console.log(this.state)
+  }
+
   componentDidMount() {
     this.fetchCategories()
+    this.fetchProducts()
   }
 
   render() {
+    let products
+    let productDetails
+    if (this.state.products.length > 0) {
+      products = this.state.products.map((product) => (
+        <div key={product.id}>
+          <button onClick={() => this.fetchProduct(product.id)}>Details</button>
+          {product.name}
+        </div>
+      ))
+    }
+    if (this.state.product) {
+      productDetails = <div>name: {this.state.product.name}</div>
+    }
+
     return (
       <div>
         <button onClick={this.fetchCategories}>Load Stuff</button>
         {this.state.categories.length > 0 &&
           this.state.categories.map((category) => (
-            <button onClick={this.fetchProducts} key={category.name}>
+            <button
+              onClick={() => this.fetchProducts(category.name)}
+              key={category.name}>
               {category.name}
             </button>
           ))}
-        {this.state.products.length > 0 &&
-          this.state.products.map((product) => (
-            <div key={product.id}>{product.id}</div>
-          ))}
+        {products}
+        {productDetails}
       </div>
     )
   }
